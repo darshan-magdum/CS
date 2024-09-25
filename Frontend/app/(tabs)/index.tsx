4c0a9788598302255ba92635e2346177ac644a2d
darshan.magdum <darshan.magdum@nihilent.com>
2024-09-25 20:49:40 +0530

    commit

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import InfoScreen from "../../components/Screens/Intro/InfoScreen";
 import SplashSceenSignup from "../../components/Screens/Intro/SplashSceenSignup";
 import Signup from "../../components/Screens/Authentication/Signup";
 import Login from "../../components/Screens/Authentication/Login";
+import AdminMenu from "../../components/Screens/Admin/AdminMenu";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -16,8 +17,12 @@ const Stack = createNativeStackNavigator();
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.Container}
       >
+      
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="SplashScreen" component={SplashScreen} />
+
+        <Stack.Screen name="AdminMenu" component={AdminMenu} />
+
+          {/* <Stack.Screen name="SplashScreen" component={SplashScreen} />
           <Stack.Screen name="InfoScreen" component={InfoScreen} />
        
           <Stack.Screen
@@ -27,7 +32,7 @@ const Stack = createNativeStackNavigator();
 
 
 <Stack.Screen name="Login" component={Login} />
-<Stack.Screen name="Signup" component={Signup} />
+<Stack.Screen name="Signup" component={Signup} /> */}
 
 
         </Stack.Navigator>
